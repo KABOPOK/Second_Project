598dd7d4a1dcd9fa7485fe36c8f8974e177d19ff
KABOPOK <mishavvvsweater@list.ru>
2022-10-14 23:54:31 +0300

    homework_2

--- task4.cpp
+++ task4.cpp
@@ -3,16 +3,38 @@ using namespace std;
 int l=0;
 int main (){
 	int n,s,p;
+	p=0;
 	cout<< "Input  n";
 	cin>>n;
-        for(int o =0; o<n; ++o){
+        for(int o =0; o<n; ++o)
+        {
+            p=0;
+            while (p<n)
+            {
 		for (int i =0; i<(n-o-1); ++i){
-                cout<<" ";}
+                cout<<" ";};
 		for (int i =0; i<(2*o+1); ++i){
         	 cout<<"*";}
-		for (int i =0; i<(n-2*o-1); ++i){
+		for (int i =0; i<(n-o-1); ++i){
 		cout<<" ";}
+		p=p+1;
+            }
         cout<<endl;
+        }
+	        for(int o =1; o<n; ++o){
+	            p=0;
+	            while (p<n)
+	            {
+                for (int i =0; i<(o); ++i){
+                cout<<" ";}
+                for (int i =0; i<(2*n-1-2*o); ++i){
+                 cout<<"*";}
+                for (int i =0; i<(o); ++i){
+                cout<<" ";}
+                p=p+1;
+	            }
+        cout<<endl;
+
         }
 
 	return 0;
