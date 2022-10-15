7b676960013962c97182617b702e448210d8b21a
KABOPOK <mishavvvsweater@list.ru>
2022-10-15 18:15:02 +0300

    It cost many time, but my neighbor help me and l does it

--- task4.cpp
+++ task4.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 using namespace std;
-int l=0;
+#define cub(a) double((a)*(a)*(a))
+#define sqr(b) double((b)*(b))
 int main (){
-	int n,s,p;
-	p=0;
-	cout<< "Input  n";
-	cin>>n;
-        for(int o =0; o<n; ++o)
+    int a,b,w,h;
+    double x,y, f;
+    w=41;
+    h=21;
+    char mass[h][w];
+    for (int i = 0; i < h; ++i)
+    {
+        for (int j = 0; j < w; ++j)
         {
-            p=0;
-            while (p<n)
-            {
-		for (int i =0; i<(n-o-1); ++i){
-                cout<<" ";};
-		for (int i =0; i<(2*o+1); ++i){
-        	 cout<<"*";}
-		for (int i =0; i<(n-o-1); ++i){
-		cout<<" ";}
-		p=p+1;
+            x=1.5-3.0*j/(w-1);
+            y=1.5-3.0*i/(h-1);
+            f = cub(sqr(x)+ sqr(y) -1) - sqr(x)*cub(y);
+            cout<<((f<=0)?  "*" :  ".");
             }
-        cout<<endl;
+            cout << endl;
         }
-	        for(int o =1; o<n; ++o){
-	            p=0;
-	            while (p<n)
-	            {
-                for (int i =0; i<(o); ++i){
-                cout<<" ";}
-                for (int i =0; i<(2*n-1-2*o); ++i){
-                 cout<<"*";}
-                for (int i =0; i<(o); ++i){
-                cout<<" ";}
-                p=p+1;
-	            }
-        cout<<endl;
 
+
+    /*    for (int i = 0; i < h; ++i) {
+        mass [i][21] = '|';
+    }
+        for (int i = 0; i < w; ++i) {
+        mass [11][i] = '-';
+    }
+     for (int i = 0; i < h; ++i)
+    {
+        for (int j = 0; j < w; ++j)
+        {
+            cout << mass[i][j];
         }
+        cout << endl;
+    }*/
+
 
 	return 0;
 }
